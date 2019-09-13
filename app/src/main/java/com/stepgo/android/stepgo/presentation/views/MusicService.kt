ada92656764abe739ca65567f2f10baa05118c33
Dimagiopatriot <troll3758@gmail.com>
2019-09-13 18:55:46 +0300

    work on music

---
+++ app/src/main/java/com/stepgo/android/stepgo/presentation/views/MusicService.kt
@@ -0,0 +1,13 @@
+package com.stepgo.android.stepgo.presentation.views
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+
+class MusicService: Service(){
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
